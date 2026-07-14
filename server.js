44bb9b0b12239794363108d2afa27f793676649b
ABHISHEK <abhishekjayamanne@gmail.com>
2026-07-15 01:34:52 +0530

    Stage 4: full CRUD

--- server.js
+++ server.js
@@ -35,18 +35,57 @@ app.get("/tasks/:id", (req, res) => {
 });
 
 app.post("/tasks", (req, res) => {
+  if (req.body.title) {
+    const lastId = Math.max(...tasks.map((task) => task.id));
+    const newTask = { id: lastId + 1, title: req.body.title, done: false };
+    tasks.push(newTask);
+    res.status(201).json(newTask);
+  } else if (req.body.title === undefined) {
+    res.status(400).json({ error: "Title is missing" });
+  } else if (req.body.title === "") {
+    res.status(400).json({ error: "Title is empty" });
+  } else {
+    res.status(400).json({ error: "Title is invalid" });
+  }
+});
+
+app.put("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const updatedTask = tasks.find((task) => task.id === id);
+
+  if (updatedTask) {
     if (req.body.title) {
-        const lastId = Math.max(...tasks.map((task) => task.id));
-        const newTask = {id: lastId + 1, title: req.body.title, done: false};
-        tasks.push(newTask);
-        res.status(201).json(newTask);
-    } else if (req.body.title === undefined) {
-         res.status(400).json({error: "Title is missing"});
-    } else if (req.body.title === "") {
-        res.status(400).json({error: "Title is empty"});
+      if (typeof req.body.done === "boolean") {
+        updatedTask.title = req.body.title;
+        updatedTask.done = req.body.done;
+        res.json(updatedTask);
+      } else if (req.body.done === undefined) {
+        updatedTask.title = req.body.title;
+        res.json(updatedTask);
+      } else {
+        res.status(400).json({ error: "Invalid done value" });
+      }
+    } else if (typeof req.body.done === "boolean") {
+      updatedTask.done = req.body.done;
+      res.json(updatedTask);
     } else {
-        res.status(400).json({error: "Title is invalid"});
+      res.status(400).json({ error: "Invalid body" });
     }
+  } else {
+    res.status(404).json({ error: `Task ${id} is not found` });
+  }
+});
+
+app.delete("/tasks/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const taskIndex = tasks.findIndex((task) => task.id === id);
+
+  if (taskIndex > -1) {
+    tasks.splice(taskIndex, 1);
+    res.status(204).send();
+  } else {
+    res.status(404).json({ error: `Task ${id} is not found` });
+  }
 });
 
 const PORT = 3000;
